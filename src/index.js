4aab03f7343c65bc49660f81e19a22bcd9102158
Tse-Hsien Chiang <jackblackevo@gmail.com>
2017-04-09 01:08:27 +0800

    remove redundant function

--- src/index.js
+++ src/index.js
@@ -3,66 +3,63 @@ import Model from './Model'
 import Controller from './Controller'
 
 window.addEventListener('DOMContentLoaded', () => {
-  ;(function pageInit() {
-    const view = new View(document)
-    const model = new Model(view)
-    const controller = new Controller(model, view)
+  const view = new View(document)
+  const model = new Model(view)
+  const controller = new Controller(model, view)
 
-    view.chooseFileInput.addEventListener('change', event => {
-      const fileList = event.target.files
+  view.chooseFileInput.addEventListener('change', event => {
+    const fileList = event.target.files
 
-      if (fileList.length > 0) {
-        const file = fileList[0]
-        controller.readSelectFile(file)
+    if (fileList.length > 0) {
+      const file = fileList[0]
+      controller.readSelectFile(file)
 
-      }
+    }
 
-    })
+  })
 
-    view.resizeImgPercentRange.addEventListener('input', event => {
-      const value = event.target.value
-      controller.updateResizeImgPercent(value)
+  view.resizeImgPercentRange.addEventListener('input', event => {
+    const value = event.target.value
+    controller.updateResizeImgPercent(value)
 
-    })
+  })
 
-    view.qualityPercentRange.addEventListener('input', event => {
-      const value = event.target.value
-      controller.updateCompressionImgPercent(value)
+  view.qualityPercentRange.addEventListener('input', event => {
+    const value = event.target.value
+    controller.updateCompressionImgPercent(value)
 
-    })
+  })
 
-    view.outputFileBtn.addEventListener('click', () => {
-      controller.getImgFile()
+  view.outputFileBtn.addEventListener('click', () => {
+    controller.getImgFile()
 
-    })
+  })
 
-    view.cropBtn.addEventListener('click', () => {
-      controller.cropImg()
+  view.cropBtn.addEventListener('click', () => {
+    controller.cropImg()
 
-    })
+  })
 
-    view.resetBtn.addEventListener('click', () => {
-      controller.resetCrop()
+  view.resetBtn.addEventListener('click', () => {
+    controller.resetCrop()
 
-    })
+  })
 
-    view.cancelBtn.addEventListener('click', () => {
-      controller.closeCrop()
+  view.cancelBtn.addEventListener('click', () => {
+    controller.closeCrop()
 
-    })
+  })
 
-    view.isGrayscaleCheckBox.addEventListener('change', event => {
-      const isGrayscale = event.target.checked
-      controller.updateGrayscaleFlag(isGrayscale)
+  view.isGrayscaleCheckBox.addEventListener('change', event => {
+    const isGrayscale = event.target.checked
+    controller.updateGrayscaleFlag(isGrayscale)
 
-    })
+  })
 
-    view.isResizeImgLockCheckBox.addEventListener('click', event => {
-      const isResizeImgLock = event.target.checked
-      controller.updateResizeImgLockFlag(isResizeImgLock)
+  view.isResizeImgLockCheckBox.addEventListener('click', event => {
+    const isResizeImgLock = event.target.checked
+    controller.updateResizeImgLockFlag(isResizeImgLock)
 
-    })
-
-  })()
+  })
 
 })
